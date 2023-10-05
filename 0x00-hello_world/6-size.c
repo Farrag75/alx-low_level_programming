c3a4f54c31b86710a6902db3c4c40b56da175839
Farrag75 <mohammed_farrag@outlook.com>
2023-10-05 15:43:59 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A c program that prints the size of different data types using sizeof function
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char c;
+	int i;
+	long int x;
+	long long int y;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(x));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(y));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
+	return (0);
+}
