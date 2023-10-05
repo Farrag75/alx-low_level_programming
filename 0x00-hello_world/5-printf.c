1c1f2d65e9fc90e075cb2c8ac4f50a6f9e7e6788
Farrag75 <mohammed_farrag@outlook.com>
2023-10-05 15:14:50 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ * Description: A c program that prints using the function printf
+ *
+ * Return: always 0 (success)
+*/
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); 
+}
